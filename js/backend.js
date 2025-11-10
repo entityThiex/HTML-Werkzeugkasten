17e32872f0e830e38ea48b47f869bc8d2879e4d1
Vincent Wolf <vincentwolf@web.de>
2025-11-10 16:56:18 +0100

    - json to html and file export wip

--- js/backend.js
+++ js/backend.js
@@ -1,15 +1,58 @@
-let data = {}
-let files = {}
-let defaultFormScript = {}
+let data = {
+    "form": {
+        "fields": [],
+        "scripts": []
+    },
+    "eval": {
+        "fields": [],
+        "scripts": []
+    }
+}
 
-function createHTMLFromJson(){
-    let fields = data["form"]["fields"];
-    let html = new htmlObject();
+let defaultData = {
+    "form": {
+        "fields": [],
+        "scripts": []
+    },
+    "eval": {
+        "fields": [],
+        "scripts": []
+    }
+}
+
+
+function createFormFromJson(){
+    return createHTMLFromJson("form");
+}
+
+function createEvalFromJson(){
+    return createHTMLFromJson("eval");
+}
+
+function createHTMLFromJson(type){
+    let fields = this.defaultData[type]["fields"].concat(data[type]["fields"]);
+    let html = new HtmlObj();
     fields.forEach(html.append());
+
+    let scripts = this.defaultData[type]["scripts"].concat(data[type]["scripts"]);
+    scripts.forEach(html.append());
+
+    return html;
 }
 
 function saveSpecificationsAsJson(){
+    let jsonSpecs = new JsonObj(this.data);
+    return jsonSpecs.getDownloadFile();
+}
 
+function saveForm(){
+    let htmlForm = createFormFromJson();
+    return htmlForm.getDownloadFile();
+}
+
+function saveEval(){
+    let htmlEval = createEvalFromJson();
+    return htmlEval.getDownloadFile();
 }
 
 function createZip(json, files){
@@ -20,24 +63,57 @@ function loadFile(){
 
 }
 
-function createHTML(){
+class Obj{
+    blob;
+    fileURL;
+    link;
 
+    constructor() {
+
+    }
+
+    getDownloadFile(data, type){ // TODO if there is a better option
+        return new Blob([data], {type: ''});
+    }
+
+    getContent(){}
 }
 
-class htmlObject{
+class HtmlObj extends Obj{
     htmlText = "";
-    blob;
 
-    constructor(){
-
+    constructor(html = ""){
+        super();
+        this.htmlText = html;
     }
 
     append(jsonHtml){
-        this.htmlText+=jsonHtml;
+        if(jsonHtml instanceof JSON && jsonHtml.has("html"));
+            this.htmlText+=jsonHtml["html"];
         return this;
     }
 
-    download(){
-        this.blob = new Blob(this.htmlText, )
+    getDownloadFile() {
+        super.getDownloadFile(this.htmlText, 'text/html');
+    }
+
+    getContent() {
+        return this.htmlText;
+    }
+}
+
+class JsonObj extends Obj{
+    json = {}
+    constructor(json) {
+        super();
+        this.json = json;
+    }
+
+    getDownloadFile() {
+        super.getDownloadFile(this.json, 'application/json');
+    }
+
+    getContent() {
+        return this.json;
     }
 }
